e6319350f6e0caf6cef6bd54afc6a23e5476a24f
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-14 20:09:49 +0900

    ADD : file add

--- src/pages/yanghoejin/LoginHoejin/LoginHoejin.js
+++ src/pages/yanghoejin/LoginHoejin/LoginHoejin.js
@@ -1,5 +1,34 @@
+import React from 'react';
+//import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import './LoginHoejin.scss';
+
 function LoginHoejin() {
-  return <div>LoginHoejin</div>;
+  const navigate = useNavigate();
+
+  function goToMain() {
+    navigate('/MainHoejin');
+  }
+
+  return (
+    <div className="loginHoejin">
+      <div className="container">
+        <h1>Westagram</h1>
+        <div className="inputandbutton">
+          <input id="name" placeholder="전화번호, 사용자 이름 또는 이메일" />
+          <input id="password" type="password" placeholder="비밀번호" />
+          <button id="button" onClick={goToMain}>
+            로그인
+          </button>
+          {/* 링크로 페이지 라우팅
+        <button id="button">
+          <Link to="/Main">로그인</Link>
+        </button> */}
+        </div>
+        <p>비밀번호를 잊으셨나요?</p>
+      </div>
+    </div>
+  );
 }
 
 export default LoginHoejin;
