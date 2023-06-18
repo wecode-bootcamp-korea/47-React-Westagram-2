eec052c473f9a8d0e8a1a221bafe0b38f94757c5
Kimsu <betnwjd25@gmail.com>
2023-06-19 00:08:28 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/kimsujeong/LoginSujeong/LoginSujeong.js
+++ src/pages/kimsujeong/LoginSujeong/LoginSujeong.js
@@ -1,27 +1,59 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './LoginSujeong.scss';
 
 const LoginSujeong = () => {
   const [ID, setID] = useState('');
   const [password, setPassword] = useState('');
-
-  const isButtonDisabled = !ID || !password;
+  const [isValidID, setIsValidID] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const navigate = useNavigate();
 
   const saveUserId = e => {
-    setID(e.target.value);
+    const value = e.target.value;
+    setID(value);
+    setIsValidID(validateID(value));
+    setIsButtonDisabled(
+      !value || !password || !validateID(value) || !isValidPassword
+    );
   };
 
   const savePassword = e => {
-    setPassword(e.target.value);
+    const value = e.target.value;
+    setPassword(value);
+    setIsValidPassword(validatePassword(value));
+    setIsButtonDisabled(
+      !ID || !value || !isValidID || !validatePassword(value)
+    );
+  };
+
+  const validateID = ID => {
+    const IDRegex = /@/;
+    return IDRegex.test(ID);
+  };
+
+  const validatePassword = password => {
+    return password.length >= 5;
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!isValidID) {
+      alert('@ 포함 여부를 확인해주세요');
+    } else if (!isValidPassword) {
+      alert('비밀번호는 5자리 이상 입력해주세요');
+    } else {
+      alert('✅ 로그인에 성공하셨습니다.');
+      navigate('/MainSujeong');
+    }
   };
 
   return (
     <div className="loginSujeong">
       <div className="content-wrap">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>westagram</h1>
           <div className="input-box">
             <input
@@ -29,6 +61,7 @@ const LoginSujeong = () => {
               type="text"
               value={ID}
               placeholder=" 전화번호, 사용자 이름 또는 이메일"
+              className={!isValidID ? 'invalid' : ''}
             />
           </div>
           <input
@@ -36,19 +69,19 @@ const LoginSujeong = () => {
             type="password"
             value={password}
             placeholder=" 비밀번호"
+            className={!isValidPassword ? 'invalid' : ''}
           />
-          <Link to="/MainSujeong">
-            <button
-              type="submit"
-              className="login-btn"
-              disabled={isButtonDisabled}
-              onClick={() => {
-                navigate('/MainSujeong');
-              }}
-            >
-              로그인
-            </button>
-          </Link>
+
+          <button
+            type="submit"
+            className="login-btn"
+            disabled={isButtonDisabled}
+            style={{
+              backgroundColor: isButtonDisabled ? 'grey' : 'rgb(45, 139, 240)',
+            }}
+          >
+            로그인
+          </button>
         </form>
 
         <a href="https://www.instagram.com/accounts/password/reset/">
