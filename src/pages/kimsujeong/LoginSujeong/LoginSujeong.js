451ff3ed82f4a3ebcdf6915d48d4fb3f5cd56be3
Kimsu <betnwjd25@gmail.com>
2023-06-18 14:35:51 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/kimsujeong/LoginSujeong/LoginSujeong.js
+++ src/pages/kimsujeong/LoginSujeong/LoginSujeong.js
@@ -1,5 +1,62 @@
-function LoginSujeong() {
-  return <div>LoginSujeong</div>;
-}
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './LoginSujeong.scss';
+
+const LoginSujeong = () => {
+  const [ID, setID] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isButtonDisabled = !ID || !password;
+  const navigate = useNavigate();
+
+  const saveUserId = e => {
+    setID(e.target.value);
+  };
+
+  const savePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="loginSujeong">
+      <div className="content-wrap">
+        <form>
+          <h1>westagram</h1>
+          <div className="input-box">
+            <input
+              onChange={saveUserId}
+              type="text"
+              value={ID}
+              placeholder=" 전화번호, 사용자 이름 또는 이메일"
+            />
+          </div>
+          <input
+            onChange={savePassword}
+            type="password"
+            value={password}
+            placeholder=" 비밀번호"
+          />
+          <Link to="/MainSujeong">
+            <button
+              type="submit"
+              className="login-btn"
+              disabled={isButtonDisabled}
+              onClick={() => {
+                navigate('/MainSujeong');
+              }}
+            >
+              로그인
+            </button>
+          </Link>
+        </form>
+
+        <a href="https://www.instagram.com/accounts/password/reset/">
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default LoginSujeong;
--- src/pages/kimsujeong/MainSujeong/MainSujeong.js
+++ src/pages/kimsujeong/MainSujeong/MainSujeong.js
@@ -1,5 +1,260 @@
-function MainSujeong() {
-  return <div>MainSujeong</div>;
-}
+import './MainSujeong.scss';
+
+const MainSujeong = () => {
+  const handleKeyUp = e => {
+    const commentInput = document.querySelector('.comment-input');
+    const commentList = document.querySelector('.comment-list');
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const commentText = commentInput.value;
+
+      if (commentText !== '') {
+        const newComment = document.createElement('li');
+        newComment.textContent = commentText;
+        commentList.appendChild(newComment);
+        commentInput.value = '';
+      }
+    }
+  };
+
+  const handleClick = () => {
+    const commentInput = document.querySelector('.comment-input');
+    const commentList = document.querySelector('.comment-list');
+
+    const commentText = commentInput.value;
+
+    if (commentText !== '') {
+      const newComment = document.createElement('li');
+      newComment.textContent = commentText;
+      commentList.appendChild(newComment);
+      commentInput.value = '';
+    } else {
+      alert('글을 작성해주세요');
+    }
+  };
+  return (
+    <div className="MainSujeong">
+      <div className="profile-detail"></div>
+      <main>
+        <div className="feeds">
+          <article>
+            <div className="posting-box">
+              <img
+                className="post-owner"
+                src="images/kimsujeong/dog.png"
+                alt="글 작성자 프로필 이미지"
+              />
+              <h4 className="owner-name">개짤만 올리는 사람</h4>
+            </div>
+            <img
+              className="posting"
+              src="images/kimsujeong/sadDog.jpeg"
+              alt="포스팅 이미지"
+            />
+            <div className="posting-icons">
+              <img
+                className="posting-icon like-icon"
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                alt="하트 아이콘"
+              />
+              <img
+                className="posting-icon comment-icon"
+                src="images/kimsujeong/comment.png"
+                alt="댓글 아이콘"
+              />
+              <img
+                className="posting-icon share-icon"
+                src="images/kimsujeong/share.png"
+                alt="공유 아이콘"
+              />
+              <img
+                className="posting-icon bookmark-icon"
+                src="images/kimsujeong/bookmark.png"
+                alt="북마크 아이콘"
+              />
+            </div>
+            <div className="posting-text">
+              <p>
+                운동하는데 묶여있는 멍멍이 봄. 주인은 맞은편에있는데 너무
+                불쌍했다.. 무려 30분이나 묶여있었어..ㅠ
+              </p>
+            </div>
+            <div>
+              <p className="small-font likes">ㅇㅇ님 외 n명이 좋아합니다.</p>
+            </div>
+            <ul className="comment-list">
+              <li className="comment-content">
+                <p>멍멍이귀여워불쌍해</p>
+                <div className="date">몇분전에 달린댓글</div>
+              </li>
+            </ul>
+            <div className="write-commments">
+              <input
+                className="comment-input"
+                type="text"
+                placeholder=" 댓글 달기..."
+                onKeyUp={handleKeyUp}
+              />
+              <button className="write-btn" onClick={handleClick}>
+                게시
+              </button>
+            </div>
+          </article>
+        </div>
+        <div className="main-right">
+          <div className="login-user">
+            <img
+              className="login-user-image"
+              src="images/kimsujeong/profile.png"
+              alt="로그인 사용자 프로필 이미지"
+            />
+            <p className="login-user-name">피카츄피카피카</p>
+          </div>
+          <div className="story">
+            <div className="small-title">
+              <span>스토리</span>
+              <span> 모두보기</span>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/dog1.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <div className="user-name">
+                닥스훈트
+                <div className="after-date">16분 전</div>
+              </div>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/dog2.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                웰시코기<div className="after-date">0분 전</div>
+              </span>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/dog3.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                시바이누<div className="after-date">1년 전</div>
+              </span>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/dog4.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                허숰희<div className="after-date">57분 전</div>
+              </span>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/dog5.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                사모예드<div className="after-date">10시간 전</div>
+              </span>
+            </div>
+          </div>
+          <div className="recommend">
+            <div className="small-title">
+              <span>회원님을 위한 추천</span>
+              <span> 모두보기</span>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/pig.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                돼지
+                <div className="recommend-like">
+                  시바이누님이 돼지를 좋아합니다.
+                </div>
+              </span>
+              <button className="follow-btn">팔로우</button>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/buger.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                햄버거킹
+                <div className="recommend-like">
+                  허숙희님이 햄버거를 좋아합니다.
+                </div>
+              </span>
+              <button className="follow-btn">팔로우</button>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/sudal.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                수달
+                <div className="recommend-like">수달님이 말봐.</div>
+              </span>
+              <button className="follow-btn">팔로우</button>
+            </div>
+            <div className="users">
+              <span>
+                <img
+                  className="user-avatars"
+                  src="images/kimsujeong/hores.png"
+                  alt="유저 프로필 이미지"
+                />
+              </span>
+              <span className="user-name">
+                말봐
+                <div className="recommend-like">뭘봐 수달.</div>
+              </span>
+              <button className="follow-btn">팔로우</button>
+            </div>
+          </div>
+          <p className="foot">
+            westagram 정보 • 지원 • 홍보 센터 • API • <br />
+            채용정보 • 개인정보처리방침 • 약관 • <br />
+            디렉터리 • 프로필 • 해시태그 • 언어
+            <br />
+            <br />@ 2023 westagram
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default MainSujeong;
